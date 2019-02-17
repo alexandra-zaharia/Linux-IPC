dbb338e1b74b411e55381da15b91c4704f0c3038
alexandra-zaharia <7894916+alexandra-zaharia@users.noreply.github.com>
2019-02-17 18:36:08 +0100

    Refactor UNIX domain servers

--- unix_domain_sockets/multiplexing/server.c
+++ unix_domain_sockets/multiplexing/server.c
@@ -29,19 +29,42 @@
 int connection_socket;                 // master socket
 
 
-// Handles SIGINT (Ctrl+C) signal by closing the server (client processes are left running)
-void shutdown_server(int sig)
-{
-    // Close master socket and release socket resource
-    close(connection_socket);
-    remove_from_monitored_fd_set(connection_socket);
-    status_message("Connection closed.");
-    unlink(SOCKET_PATH);
-    exit(EXIT_SUCCESS);
-}
+void initialize_server();
+void handle_connection_initiation_request();
+void handle_service_request(fd_set read_fds, char* buffer);
+void shutdown_server(int sig);
 
 
 int main()
+{
+    initialize_server();
+
+    fd_set read_fds;
+    char buffer[BUFFER_SIZE];
+
+    for (;;) { // handle client connections iteratively
+        refresh_fd_set(&read_fds); // copy all file descriptors to read_fds
+
+        // Server process blocks on select() system call
+        status_message("Waiting on select() system call.");
+        select(get_max_fd() + 1, &read_fds, NULL, NULL, NULL);
+
+        if (FD_ISSET(connection_socket, &read_fds)) { // connection initiation request from client
+            handle_connection_initiation_request();
+        } else if (FD_ISSET(0, &read_fds)) { // console input on the server side
+            memset(buffer, 0, BUFFER_SIZE);
+            if (read(0, buffer, BUFFER_SIZE) == -1)
+                error_and_exit("Cannot read data from console.");
+            printf("Input read from console: %s\n", buffer);
+        } else { // data arrives on client file descriptor
+            handle_service_request(read_fds, buffer);
+        }
+    }
+}
+
+
+// Initializes the server
+void initialize_server()
 {
     // Remove the socket in case the program had previously exited abnormally
     unlink(SOCKET_PATH);
@@ -76,61 +99,66 @@ int main()
 
     signal(SIGINT, shutdown_server); // register SIGINT handler to shutdown server cleanly
     signal(SIGPIPE, SIG_IGN); // ignore SIGPIPE: when client disconnects without waiting for message
+}
 
-    fd_set readfds;
-    char buffer[BUFFER_SIZE];
-    for (;;) { // handle client connections iteratively
-        refresh_fd_set(&readfds); // copy all file descriptors to read_fds
 
-        // Server process blocks on select() system call
-        status_message("Waiting on select() system call.");
-        select(get_max_fd() + 1, &readfds, NULL, NULL, NULL);
+// Accepts a new connection from a client
+void handle_connection_initiation_request()
+{
+    status_message("New connection received, accepting the connection.");
+    int data_socket = accept(connection_socket, NULL, NULL);
+    if (data_socket == -1)
+        error_and_exit("Cannot accept client connection.");
+    status_message("Connection accepted from client.");
+    add_to_monitored_fd_set(data_socket);
+}
 
-        if (FD_ISSET(connection_socket, &readfds)) { // connection initiation request from client
-            status_message("New connection received, accepting the connection.");
-            int data_socket = accept(connection_socket, NULL, NULL);
-            if (data_socket == -1)
-                error_and_exit("Cannot accept client connection.");
-            status_message("Connection accepted from client.");
-            add_to_monitored_fd_set(data_socket);
-        } else if (FD_ISSET(0, &readfds)) {
+
+// Handles service requests from clients
+void handle_service_request(fd_set read_fds, char* buffer)
+{
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (FD_ISSET(monitored_fd_set[i], &read_fds)) {
+            int comm_socket_fd = monitored_fd_set[i];
+
+            // Prepare the buffer to receive the data
             memset(buffer, 0, BUFFER_SIZE);
-            if (read(0, buffer, BUFFER_SIZE) == -1)
-                error_and_exit("Cannot read data from console.");
-            printf("Input read from console: %s\n", buffer);
-        } else { // data arrives on client file descriptor
-            for (int i = 0; i < MAX_CLIENTS; i++) {
-                if (FD_ISSET(monitored_fd_set[i], &readfds)) {
-                    int comm_socket_fd = monitored_fd_set[i];
 
-                    // Prepare the buffer to receive the data
-                    memset(buffer, 0, BUFFER_SIZE);
+            // Wait for next data packet ('read' is a blocking system call)
+            status_message("Waiting for data from the client.");
+            if (read(comm_socket_fd, buffer, BUFFER_SIZE) == -1)
+                error_and_exit("Cannot read data from client.");
 
-                    // Wait for next data packet ('read' is a blocking system call)
-                    status_message("Waiting for data from the client.");
-                    if (read(comm_socket_fd, buffer, BUFFER_SIZE) == -1)
-                        error_and_exit("Cannot read data from client.");
+            // Add received data to sum
+            int data;
+            memcpy(&data, buffer, sizeof(int));
+            if (data == 0) {
+                // Send the result to the client
+                memset(buffer, 0, BUFFER_SIZE);
+                sprintf(buffer, "Result = %d", client_result[i]);
+                status_message("Sending final result back to client.");
+                if (write(comm_socket_fd, buffer, BUFFER_SIZE) == -1)
+                    error_message("Cannot send sum to client.");
 
-                    // Add received data to sum
-                    int data;
-                    memcpy(&data, buffer, sizeof(int));
-                    if (data == 0) {
-                        // Send the result to the client
-                        memset(buffer, 0, BUFFER_SIZE);
-                        sprintf(buffer, "Result = %d", client_result[i]);
-                        status_message("Sending final result back to client.");
-                        if (write(comm_socket_fd, buffer, BUFFER_SIZE) == -1)
-                            error_message("Cannot send sum to client.");
-
-                        // Close client socket
-                        close(comm_socket_fd);
-                        client_result[i] = 0;
-                        remove_from_monitored_fd_set(comm_socket_fd);
-                        continue; // go to select and block
-                    }
-                    client_result[i] += data;
-                }
+                // Close client socket
+                close(comm_socket_fd);
+                client_result[i] = 0;
+                remove_from_monitored_fd_set(comm_socket_fd);
+                continue; // go to select and block
             }
+            client_result[i] += data;
         }
     }
+}
+
+
+// Handles SIGINT (Ctrl+C) signal by closing the server (client processes are left running)
+void shutdown_server(int sig)
+{
+    // Close master socket and release socket resource
+    close(connection_socket);
+    remove_from_monitored_fd_set(connection_socket);
+    status_message("Connection closed.");
+    unlink(SOCKET_PATH);
+    exit(EXIT_SUCCESS);
 }
--- unix_domain_sockets/no_multiplexing/server.c
+++ unix_domain_sockets/no_multiplexing/server.c
@@ -28,19 +28,38 @@
 int connection_socket;                 // master socket
 
 
-// Handles SIGINT (Ctrl+C) signal by closing the server (client process is left running)
-void shutdown_server(int sig)
-{
-    // Close master socket and release socket resource
-    close(connection_socket);
-    status_message("Connection closed.");
-    unlink(SOCKET_PATH);
-
-    exit(EXIT_SUCCESS);
-}
+void initialize_server();
+int handle_connection_initiation_request();
+void handle_service_request(int data_socket, char* buffer, int* sum);
+void shutdown_server(int sig);
 
 
 int main()
+{
+    initialize_server();
+
+    for (;;) { // handle client connections iteratively
+        int data_socket = handle_connection_initiation_request();
+
+        int sum = 0;
+        char buffer[BUFFER_SIZE];
+        handle_service_request(data_socket, buffer, &sum);
+
+        // Send the result to the client
+        memset(buffer, 0, BUFFER_SIZE);
+        sprintf(buffer, "Result = %d", sum);
+        status_message("Sending final result back to client.");
+        if (write(data_socket, buffer, BUFFER_SIZE) == -1)
+            error_message("Cannot send sum to client.");
+
+        // Close client socket
+        close(data_socket);
+    }
+}
+
+
+// Initializes the server
+void initialize_server()
 {
     // Remove the socket in case the program had previously exited abnormally
     unlink(SOCKET_PATH);
@@ -68,43 +87,50 @@ int main()
 
     signal(SIGINT, shutdown_server); // register SIGINT handler to shutdown server cleanly
     signal(SIGPIPE, SIG_IGN); // ignore SIGPIPE: when client disconnects without waiting for message
+}
 
-    for (;;) { // handle client connections iteratively
-        /* Accept a connection on a new socket ('data_socket'). The listening socket
-         * ('connection_socket') remains open and can be used to accept further connections. */
-        status_message("Waiting on accept() system call.");
-        int data_socket = accept(connection_socket, NULL, NULL);
-        if (data_socket == -1)
-            error_and_exit("Cannot accept client connection.");
-        status_message("Connection accepted from client.");
 
-        int sum = 0;
-        char buffer[BUFFER_SIZE];
-        for (;;) {
-            // Prepare the buffer to receive the data
-            memset(buffer, 0, BUFFER_SIZE);
+// Accepts a new connection from a client and returns the file descriptor for the new socket.
+int handle_connection_initiation_request()
+{
+    status_message("Waiting on accept() system call.");
+    int data_socket = accept(connection_socket, NULL, NULL);
+    if (data_socket == -1)
+        error_and_exit("Cannot accept client connection.");
+    status_message("Connection accepted from client.");
+    return data_socket;
+}
 
-            // Wait for next data packet ('read' is a blocking system call)
-            status_message("Waiting for data from the client.");
-            if (read(data_socket, buffer, BUFFER_SIZE) == -1)
-                error_and_exit("Cannot read data from client.");
 
-            // Add received data to sum
-            int data;
-            memcpy(&data, buffer, sizeof(int));
-            if (data == 0)
-                break;
-            sum += data;
-        }
-
-        // Send the result to the client
+// Handles service requests from a client using the specified socket file descriptor.
+void handle_service_request(const int data_socket, char* buffer, int* sum)
+{
+    for (;;) {
+        // Prepare the buffer to receive the data
         memset(buffer, 0, BUFFER_SIZE);
-        sprintf(buffer, "Result = %d", sum);
-        status_message("Sending final result back to client.");
-        if (write(data_socket, buffer, BUFFER_SIZE) == -1)
-            error_message("Cannot send sum to client.");
 
-        // Close client socket
-        close(data_socket);
+        // Wait for next data packet ('read' is a blocking system call)
+        status_message("Waiting for data from the client.");
+        if (read(data_socket, buffer, BUFFER_SIZE) == -1)
+            error_and_exit("Cannot read data from client.");
+
+        // Add received data to sum
+        int data;
+        memcpy(&data, buffer, sizeof(int));
+        if (data == 0)
+            break;
+        *sum += data;
     }
+}
+
+
+// Handles SIGINT (Ctrl+C) signal by closing the server (client process is left running)
+void shutdown_server(int sig)
+{
+    // Close master socket and release socket resource
+    close(connection_socket);
+    status_message("Connection closed.");
+    unlink(SOCKET_PATH);
+
+    exit(EXIT_SUCCESS);
 }
