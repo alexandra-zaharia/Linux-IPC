1b0d0e808a8b2ba2b9f2bf9b7dad871ae029e1ed
alexandra-zaharia <7894916+alexandra-zaharia@users.noreply.github.com>
2019-02-20 08:13:37 +0100

    Create RTM server menu skeleton

--- routing_table_manager/src/rtm_server.c
+++ routing_table_manager/src/rtm_server.c
@@ -8,9 +8,53 @@
 #include "rtm.h"
 #include "input.h"
 
+
+char read_routing_menu_choice(RoutingTable *rtm);
+
+
 int main()
 {
     printf("server\n");
+    RoutingTable* rtm = routing_table_create();
+
+    char option;
+    do {
+        option = read_routing_menu_choice(rtm);
+        switch (option) {
+            case 'c':
+            case 'C': {
+                printf("[creating a record...]\n");
+                rtm->size++; // TODO remove
+            }; break;
+            case 'u':
+            case 'U': {
+                if (rtm->size == 0) {
+                    printf("Unknown option '%c'.\n", option);
+                    break;
+                }
+                printf("[updating a record...]\n");
+            }; break;
+            case 'd':
+            case 'D': {
+                if (rtm->size == 0) {
+                    printf("Unknown option '%c'.\n", option);
+                    break;
+                }
+                printf("[deleting a record...]\n");
+            }; break;
+            case 'p':
+            case 'P': {
+                if (rtm->size == 0) {
+                    printf("Unknown option '%c'.\n", option);
+                    break;
+                }
+                printf("[printing table...]\n");
+            }; break;
+            case 'q':
+            case 'Q': printf("Bye!\n"); break;
+            default: printf("Unknown option '%c'.\n", option);
+        }
+    } while (option != 'q' && option != 'Q');
 
     char ip_addr[IP_ADDR_LEN];
     printf("Enter IP address (xxx.xxx.xxx.xxx): ");
@@ -25,4 +69,26 @@ int main()
         printf("invalid\n");
     }
 
+    routing_table_free(rtm);
+}
+
+
+char read_routing_menu_choice(RoutingTable *rtm)
+{
+    if (rtm->size == 0) {
+        printf("The routing table has no entries. Available options:\n");
+        printf("\tc - Create a record\n");
+        printf("\tq - Quit\n");
+    } else {
+        printf("The routing table has %d %s. Available options:\n",
+               rtm->size, rtm->size == 1 ? "entry" : "entries");
+        printf("\tc - Create a record\n");
+        printf("\tu - Update a record\n");
+        printf("\td - Delete a record\n");
+        printf("\tp - Print the routing table contents\n");
+        printf("\tq - Quit\n");
+    }
+
+    char buffer[2];
+    return read_line(buffer, 2) == -1 ? (char) 'q' : buffer[0];
 }
