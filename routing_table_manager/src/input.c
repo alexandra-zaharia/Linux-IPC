a288f8fdee325b6a1c512413df5f34e6f9da82d3
azaharia <7894916+alexandra-zaharia@users.noreply.github.com>
2019-02-21 17:19:28 +0100

    Replace strtok with strsep for handling empty tokens

--- routing_table_manager/src/input.c
+++ routing_table_manager/src/input.c
@@ -2,6 +2,7 @@
 // Created by Alexandra Zaharia on 16/02/19.
 //
 
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include "rtm.h"
@@ -10,8 +11,8 @@
 
 
 /*
- * Reads an IPv4 address from STDIN, in the format xxx.xxx.xxx.xxx. Returns 0 on success and -1 on
- * failure.
+ * Reads an IPv4 address from STDIN and stores it into the `ip_address` buffer, in the form
+ * xxx.xxx.xxx.xxx. Returns 0 on success and -1 on failure.
  *
  * In order for an IPv4 address to be valid, it needs:
  *     - to contain precisely 3 '.' (dot) symbols
@@ -26,37 +27,44 @@ int read_ip_address_from_stdin(char *ip_address)
 
 
 /*
- * Reads an IPv4 address from the buffer, in the format xxx.xxx.xxx.xxx. Returns 0 on success and -1
- * on failure. Valid IPv4 addresses follow the same rules as for read_ip_address_from_stdin.
+ * Reads an IPv4 address from the `ip_address` buffer, in the form xxx.xxx.xxx.xxx. Returns 0 on
+ * success and -1 on failure. Valid IPv4 addresses follow the same rules as for
+ * `read_ip_address_from_stdin`.
  */
 int read_ip_address_from_buffer(char *ip_address)
 {
-    if (!ip_address) return -1;
-    if (strchr(ip_address, '.') == NULL) return -1;
+    if (!ip_address || strchr(ip_address, '.') == NULL) return -1;
 
-    char buffer[IP_ADDR_LEN];
-    strncpy(buffer, ip_address, IP_ADDR_LEN);
-    char *token = strtok(buffer, ".");
+    /* Create a copy of the input string to work on (`buffer`), since it gets modified by `strsep`.
+     * A reference to the beginning of the buffer needs to be kept (`head`) in order to ensure
+     * correct de-allocation. */
+    char *buffer = strndup(ip_address, IP_ADDR_LEN); // work on a copy of the input string
+    char *head = buffer;                             // keep a reference to the head of the buffer
+    char *token;
     int gp_count = 0;
 
-    while (token) {
+    while ((token = strsep(&buffer, ".")) != NULL) {
         int ip_gp;
-        if (read_int_from_buffer(token, &ip_gp) == -1) return -1;
-        if (ip_gp < IP_ADDR_MIN || ip_gp > IP_ADDR_MAX) return -1;
+        if (read_int_from_buffer(token, &ip_gp) == -1) goto clean_fail;
+        if (ip_gp < IP_ADDR_MIN || ip_gp > IP_ADDR_MAX) goto clean_fail;
         ++gp_count;
-        token = strtok(NULL, ".");
     }
+    free(head);                                      // when here, `buffer` is NULL
+    head = NULL;
+    if (gp_count == IP_ADDR_GP) return 0;
 
-    if (gp_count != IP_ADDR_GP) return -1;
-
-    return 0;
+clean_fail:
+    if (head) free(head);
+    return -1;
 }
 
 
 /*
- * Reads a destination subnet in the form xxx.xxx.xxx.xxx/yy, where xxx.xxx.xxx.xxx is an IPv4
- * address that follows the same rules as for read_ip_address_from_stdin, and yy is the subnet mask
- * (an integer between 0 and 32). Returns 0 on success and -1 on failure.
+ * Reads a destination subnet from the `dst_subnet` buffer in the form xxx.xxx.xxx.xxx/yy, where
+ * xxx.xxx.xxx.xxx is an IPv4 address that follows the same rules as for
+ * `read_ip_address_from_stdin`, and yy is the subnet mask (an integer between 0 and 32). The IPv4
+ * address is stored in the `ip_address` buffer, and the subnet mask is stored in the u16 variable
+ * pointed to by `mask`. Returns 0 on success and -1 on failure.
  */
 int read_destination_subnet(char *dst_subnet, char *ip_address, u16 *mask)
 {
