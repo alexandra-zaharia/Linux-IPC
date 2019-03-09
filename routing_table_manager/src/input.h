d99e35613c1d41333f59bcf316a6c05722b71992
alexandra-zaharia <7894916+alexandra-zaharia@users.noreply.github.com>
2019-03-09 10:12:17 +0100

    Move IP_ADDR definitions from input.h to rtm.h

--- routing_table_manager/src/input.h
+++ routing_table_manager/src/input.h
@@ -7,11 +7,6 @@
 
 #include "rtm.h"
 
-#define IP_ADDR_LEN 16  // maximum length of an IPv4 address, including the null character
-#define IP_ADDR_GP 4    // number of dot-separated groups in an IPv4 address
-#define IP_ADDR_MIN 0   // minimum value of a dot-separated group in an IPv4 address
-#define IP_ADDR_MAX 255 // maximum value of a dot-separated group in an IPv4 address
-
 int read_ip_address_from_stdin(char *ip_address);
 int read_ip_address_from_buffer(char *ip_address);
 int read_destination_subnet_from_stdin(char *dst_subnet, char *ip_address, u16 *mask);
--- routing_table_manager/src/rtm.h
+++ routing_table_manager/src/rtm.h
@@ -14,10 +14,13 @@ typedef enum { // operation code for possible actions to be taken on the routing
 } OP_CODE;
 
 typedef unsigned short u16;
-#define MASK_MIN 0  // minimum value of destination subnet mask
-#define MASK_MAX 32 // maximum value of destination subnet mask
-
-#define OIF_LEN 32  // maximum length of outgoing interface string, including null character
+#define MASK_MIN 0         // minimum value of destination subnet mask
+#define MASK_MAX 32        // maximum value of destination subnet mask
+#define IP_ADDR_LEN 16     // maximum length of an IPv4 address, including the null character
+#define IP_ADDR_GP 4       // number of dot-separated groups in an IPv4 address
+#define IP_ADDR_MIN 0      // minimum value of a dot-separated group in an IPv4 address
+#define IP_ADDR_MAX 255    // maximum value of a dot-separated group in an IPv4 address
+#define OIF_LEN 32         // maximum length of outgoing interface string, including null character
 
 typedef struct _msg_body { // RTM entry to create, update, or delete
     char destination[16];
