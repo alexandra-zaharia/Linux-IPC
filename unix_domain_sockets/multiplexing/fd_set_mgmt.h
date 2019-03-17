677b7830ab6f5c44ed93d22546dcbf70f9ae4398
alexandra-zaharia <7894916+alexandra-zaharia@users.noreply.github.com>
2019-03-17 21:49:30 +0100

    Format comments

--- unix_domain_sockets/multiplexing/fd_set_mgmt.h
+++ unix_domain_sockets/multiplexing/fd_set_mgmt.h
@@ -10,12 +10,12 @@
 #define MAX_CLIENTS 32 // maximum number of supported clients + 1 for the master socket
 
 extern int monitored_fd_set[MAX_CLIENTS]; // file descriptors maintained by the server process
-extern int client_result[MAX_CLIENTS]; // intermediary results for client processes
+extern int client_result[MAX_CLIENTS];    // intermediary results for client processes
 
-void initialize_monitor_fd_set(); // remove all file descriptors from the monitored fd_set
-void add_to_monitored_fd_set(int); // add a new file descriptor to the monitored fd_set
-void remove_from_monitored_fd_set(int); // remove a file descriptor from the monitored fd_set
-void refresh_fd_set(fd_set *fd_set_ptr); // clone all file descriptors into the fd_set
+void initialize_monitor_fd_set();         // remove all file descriptors from the monitored fd_set
+void add_to_monitored_fd_set(int);        // add a new file descriptor to the monitored fd_set
+void remove_from_monitored_fd_set(int);   // remove a file descriptor from the monitored fd_set
+void refresh_fd_set(fd_set *fd_set_ptr);  // clone all file descriptors into the fd_set
 int get_max_fd(); // return the file descriptor in the monitored fd_set with the max value
 
 #endif //LINUX_IPC_FD_SET_MGMT_H
